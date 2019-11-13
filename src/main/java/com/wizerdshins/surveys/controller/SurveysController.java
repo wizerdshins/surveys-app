5dc4204091bb7726d5ab4241bcd52de6d26d493c
wizerdshins <shezarr@yandex.ru>
2019-11-13 22:18:43 +0900

    add swagger

--- src/main/java/com/wizerdshins/surveys/controller/SurveysController.java
+++ src/main/java/com/wizerdshins/surveys/controller/SurveysController.java
@@ -5,8 +5,11 @@ import com.wizerdshins.surveys.domain.Survey;
 import com.wizerdshins.surveys.repository.QuestionRepository;
 import com.wizerdshins.surveys.repository.SurveyRepository;
 import com.wizerdshins.surveys.service.SurveyService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -14,7 +17,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/survey")
+@Api(value = "Survey Controller", description = "Main controller allows to use basic CRUD operations")
 public class SurveysController {
 
     private SurveyRepository surveyRepository;
@@ -29,6 +33,10 @@ public class SurveysController {
         this.surveyService = surveyService;
     }
 
+    @ApiOperation(value = "Show all surveys. " +
+            "Tag 'title' or 'data' will be sorting results (f.e. '/tag=?'). " +
+            "It's not required",
+            response = Page.class)
     @GetMapping("show{tag}")
     public Page<Survey> getAll(@PageableDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
                                @RequestParam(name = "tag", defaultValue = "", required = false) String tag) {
@@ -37,17 +45,19 @@ public class SurveysController {
         return surveyService.showAll(pageable, tag);
     }
 
+    @ApiOperation(value = "Get Survey by ID", response = Survey.class)
     @GetMapping("{id}")
     public Survey getOne(@PathVariable("id") Survey survey) {
         return survey;
     }
 
+    @ApiOperation(value = "Add new Survey")
     @PostMapping("add/survey")
     public Survey addSurvey(@RequestBody Survey survey) {
         return surveyRepository.save(survey);
     }
 
-
+    @ApiOperation(value = "Add new Question; param ID points to parent Survey")
     @PostMapping("add/question/{id}")
     public Question addQuestion(@PathVariable("id") Long surveyId,
                                 @RequestBody Question question) {
@@ -55,6 +65,7 @@ public class SurveysController {
         return questionRepository.save(question);
     }
 
+    @ApiOperation(value = "Updated Survey by ID")
     @PostMapping("update/{id}")
     public Survey edit(@PathVariable("id") Survey persistSurvey,
                        @RequestBody Survey updatedSurvey) {
@@ -62,6 +73,7 @@ public class SurveysController {
         return surveyRepository.save(persistSurvey);
     }
 
+    @ApiOperation(value = "Delete Survey by ID")
     @DeleteMapping("delete/{id}")
     public void delete(@PathVariable("id") Survey survey) {
         surveyRepository.delete(survey);
--- src/main/java/com/wizerdshins/surveys/repository/SurveyRepository.java
+++ src/main/java/com/wizerdshins/surveys/repository/SurveyRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
-
 
     @Query("from Survey s order by s.title asc")
     Page<Survey> findAllOrderByTitle(Pageable pageable);
